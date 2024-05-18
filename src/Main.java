8c41fcdb198e39218b3c32e61cc0beed8f46fab2
Fox-Why-Not <Clover.kos.1@gmail.com>
2024-05-18 20:23:53 +0300

    q

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Task-1");
 
-        String firstName = "Ivan ";
-        String middleName ="Ivanov ";
-        String lastName = "Ivanovich ";
-        String fullName = middleName + firstName + lastName;
+        System.out.println("Task-1");
+String middleName ="Ivanov";
+String firstName = "Ivan";
+String lastName = "Ivanovich";
+    String fullName = middleName + " " + firstName + " " + lastName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
-        System.out.println();
+            System.out.println();
 
         System.out.println("Task-2");
-        System.out.println(fullName.toUpperCase());
-        System.out.println();
+System.out.println(fullName.toUpperCase());
+    System.out.println();
 
         System.out.println("Task-3");
-        String fullName2 = "Иванов Семён Семёнович";
-        String finalFullName2 = fullName2.replace('ё', 'е');
+String fullName2 = "Иванов Семён Семёнович";
+    String finalFullName2 = fullName2.replace('ё', 'е');
         System.out.println("Данные Ф. И. О. сотрудника — " + finalFullName2);
     }
 }
